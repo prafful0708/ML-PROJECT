4aa63afa53c5b4047fb35776a6b1e9baaefb4816
prafful0708 <prafulltrivedi1256@gmail.com>
2026-05-25 19:29:33 +0530

    Data Ingestion

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-import logging
+from src.logger import logging
 
 def error_message_detail(error, error_detail: sys):
     _, _, exc_tb = error_detail.exc_info()
@@ -35,6 +35,7 @@ if __name__ == "__main__":
         a = 1 / 0
 
     except Exception as e:
-        logging.info("Divide by zero error")
+
+        logging.error("Divide by zero error", exc_info=True)
 
         raise CustomException(e, sys)
